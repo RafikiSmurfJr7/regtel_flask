3e610c73c469415c1af1305946da3edde56aaccc
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2023-02-16 15:16:01 +0000

    modificações nos routes implementação de dados

--- regtel/routes.py
+++ regtel/routes.py
@@ -1,33 +1,74 @@
 from regtel import app, db
 from regtel.extensions import *
 from regtel.models import Registo
-from datetime import date
+from datetime import date, datetime
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-
 
     if request.method == 'GET':
 
         registo = db.session.execute(db.select(Registo).order_by(Registo.data_registo)).scalars()
 
-
         return render_template('home.html' ,data=registo)
     
     elif request.method == 'POST':
 
+        if request.form["dataNasc"] != '':
+                dataNasc = datetime.strptime(request.form["dataNasc"],'%Y-%m-%d')
 
-        registo = Registo(
-            nome = request.form["nome"],
-            funcao = request.form["funcao"],
-            entidade = request.form["entidade"],
-            numero = request.form["nTelPessoal"],
-            email = request.form["email"],
-            data_registo = date.today()
-        )
+                registo = Registo(
+                    nome = request.form["nome"],
+                    email = request.form["email"],
+
+                    data_nasc = dataNasc,
+                    
+                    numero = request.form["nTelPessoal"],
+                    funcao = request.form["funcao"],
+                    entidade = request.form["entidade"],
+                    data_registo = date.today()
+                )
+        else:
+                registo = Registo(
+                    nome = request.form["nome"],
+                    email = request.form["email"],
+                    numero = request.form["nTelPessoal"],
+                    funcao = request.form["funcao"],
+                    entidade = request.form["entidade"],
+                    data_registo = date.today()
+                )
+
 
         db.session.add(registo)
         db.session.commit()
 
 
         return redirect("/")
+    
+
+@app.route("/delete/<int:id>")
+def delete_record(id):
+    
+    record = db.one_or_404(db.select(Registo).filter_by(id=id))
+    db.session.delete(record)
+    db.session.commit()
+
+    return redirect("/")
+
+
+@app.route("/profile/<int:id>")
+def profile(id):
+    
+    record = db.one_or_404(db.select(Registo).filter_by(id=id))
+    
+    return render_template("profile.html" ,data=record)
+
+@app.route("/profile/edit/about/<int:id>", methods=['POST'])
+def profile_edit_about(id):
+        
+        descricao = request.form['text']
+
+        record = db.get_or_404(Registo, id)
+        record.descricao = descricao
+        db.session.commit()
+        return "succeed"
