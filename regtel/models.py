56276d800621f9a90fdc1950c17da5bdcdde6719
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2023-02-16 15:15:29 +0000

    edição dos models

--- regtel/models.py
+++ regtel/models.py
@@ -3,8 +3,11 @@ from regtel import db
 class Registo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50),nullable=False)
-    funcao = db.Column(db.String(50))
-    entidade = db.Column(db.String(50))
-    numero = db.Column(db.Integer, unique=True)
+    numero = db.Column(db.Integer, unique=True, nullable=False)
+    data_nasc = db.Column(db.Date)
     email = db.Column(db.String(50), unique=True)
+    funcao = db.Column(db.String(50))
+    entidade = db.Column(db.String(50), nullable=False)
+    descricao = db.Column(db.String(100))
     data_registo = db.Column(db.Date)
+
