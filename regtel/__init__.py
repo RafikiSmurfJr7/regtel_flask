157058d4b49c376417c54a7a71ad555ae7bf1c2c
Rafael Duarte Mauricio Correia <a55910@alunos.ipb.pt>
2023-02-20 23:39:19 +0000

    adição de secret key e pequenas alterações

--- regtel/__init__.py
+++ regtel/__init__.py
@@ -1,10 +1,10 @@
 from regtel.extensions import Flask,SQLAlchemy
-
+from regtel.config import Config
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.sqlite"
 db.init_app(app)
